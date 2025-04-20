ad6b21ef0c62f2c19f2a26ad7006c77fc0d0c1b0
stream <yanamarco017@gmail.com>
2025-04-19 23:28:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 1. Inicialización de componentes
     initAOS();
     initCurrencyConverter(); // Mejorado
-    //initScrollEffects();
+    initScrollEffects();
     initCategoryFilters();
     initRippleEffects();
     initTypingEffect();
